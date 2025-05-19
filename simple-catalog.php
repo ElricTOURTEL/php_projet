ca806fce8834c3118dd4ab65057fd3de4e32711f
ElricTOURTEL <tourtel.elric@gmail.com>
2025-05-19 15:43:13 +0200

    feat(display): affichage produit

---
+++ simple-catalog.php
@@ -0,0 +1,16 @@
+<?php include('header.php');?>
+
+<?php
+$products= ["Iphone", "Galaxy S25", "Samsung Galaxy zfold"];
+array_multisort($products);
+$lastproduct=count($products);
+echo $products[$lastproduct-1] . "<br>";
+for ($i = count($products); $i >= 0; $i--){
+    echo $products[$i] . "<br>";
+}
+for ($i=0; $i < count($products); $i++){
+    echo $products[$i] . "<br>";
+}
+?>
+
+<?php include('footer.php')?>
